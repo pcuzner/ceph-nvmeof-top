bd51e3b7fa23fe374752916aa838dbec2c272197
Paul Cuzner <pcuzner@ibm.com>
2024-03-12 16:08:04 +1300

    ui: Declare main classes to simplify imports
    Signed-off-by: Paul Cuzner <pcuzner@ibm.com>

---
+++ nvmeof_top/ui/__init__.py
@@ -0,0 +1,8 @@
+
+from .common import palette  # noqa: F401
+from .header import Header  # noqa: F401
+from .namespaces import NamespaceTable  # noqa: F401
+from .subsystem import SubsystemInfo  # noqa: F401
+from .help import HelpInformation  # noqa: F401
+from .options import Options  # noqa: F401
+from .cpustats import CPUStats  # noqa: F401
