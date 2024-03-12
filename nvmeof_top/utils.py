269e85ba38511c28221f3f6b7365ddae2af5ac26
Paul Cuzner <pcuzner@ibm.com>
2024-03-12 16:02:22 +1300

    core: update common utils used across the package
    Additional functions for SSL handling added
    
    Signed-off-by: Paul Cuzner <pcuzner@ibm.com>

--- nvmeof_top/utils.py
+++ nvmeof_top/utils.py
@@ -1,13 +1,63 @@
+import os
 import sys
 import uuid
 import regex
-import argparse
 import logging
+import yaml
+from typing import Tuple
 logger = logging.getLogger(__name__)
 
 
+def certs_ok(args) -> Tuple[bool, str]:
+
+    ssl_cert_files = [args.server_cert, args.client_cert, args.client_key]
+
+    # if none of the cert options are set just return true
+    if not args.server_cert and not args.client_cert and not args.client_key:
+        return True, "ok"
+
+    # if any of the server/client/key paramters are set they must all be
+    if not all(ssl_cert_files) and not args.ssl_config:
+        return False, "For a secure connection to a gateway, all SSL parameters are required: server-cert, client-cert, client-key"
+
+    # if ssl-config is given and any of the certs - abort
+    if args.ssl_config and any(ssl_cert_files):
+        return False, "Use ssl-config OR individual cert key file names, not both"
+
+    if all(ssl_cert_files):
+        if not all([os.path.exists(f) for f in ssl_cert_files]):
+            return False, "One or more cert files not found, please check the paths provided"
+
+    # if server/client/key parameters ensure the files all exist
+    if args.ssl_config:
+        ok, msg = validate_ssl_config()
+        if not ok:
+            return False, msg
+    return True, "ok"
+
+
+def validate_ssl_config(filename: str) -> Tuple[bool, str]:
+    required_keys = ['server-cert', 'client-cert', 'client-key']
+
+    if not os.path.exists(filename):
+        return False, f"{filename} does not exist"
+    if not os.path.isfile(filename):
+        return False, f"{filename} exists, but is not a file"
+
+    with open(file=filename, mode='r') as f:
+        try:
+            data = yaml.safe_load(f)
+        except yaml.YAMLError:
+            return False, f"{filename} contains invalid YAML"
+
+    if not all([key in data for key in required_keys]):
+        return False, f"{filename} must contain keys: {','.join(required_keys)}"
+
+    return True, "ok"
+
+
 def abort(rc: int, msg: str):
-    logger.critical(f"nvmeof-top has hit a problem: {msg}")
+    logger.critical(f"nvmeof-top has encountered an error: {msg}")
     print(msg)
     sys.exit(rc)
 
@@ -29,26 +79,26 @@ def valid_nqn(nqn: str) -> str:
     #   nqn.2014-08.org.nvmexpress:uuid:ee889718-8c69-40d3-8e78-5be049f966a6
 
     if not 11 < len(nqn) < 224:
-        raise argparse.ArgumentTypeError("nqn length is invalid. must be between 11-223 characters in length")
+        raise ValueError("nqn length is invalid. must be between 11-223 characters in length")
 
     if not nqn.startswith("nqn."):
-        raise argparse.ArgumentTypeError("nqn name must begin with nqn")
+        raise ValueError("nqn name must begin with nqn")
 
     dot_qualifiers = nqn.split('.')
     if not nqn.count('.') == 3:
-        raise argparse.ArgumentTypeError("nqn must consist of 3 main qualifiers")
+        raise ValueError("nqn must consist of 3 main qualifiers")
 
     valid_date = regex.findall(r"\d{4}-\d{2}", dot_qualifiers[1])  # YYYY-MM
     if not valid_date:
-        raise argparse.ArgumentTypeError("The 2nd qualifer of an nqn must be of the form YYYY-MM")
+        raise ValueError("The 2nd qualifer of an nqn must be of the form YYYY-MM")
 
     colon_qualifiers = nqn.split(':')
     if not 0 < len(colon_qualifiers) < 3:
-        raise argparse.ArgumentTypeError("a valid nqn has either 1 or 2 ':' symbols")
+        raise ValueError("a valid nqn has either 1 or 2 ':' symbols")
 
     if len(colon_qualifiers) == 3:
         if colon_qualifiers[1] == 'uuid' and not valid_uuid(colon_qualifiers[2]):
-            raise argparse.ArgumentTypeError("nqn contains an invalid uuid suffix")
+            raise ValueError("nqn contains an invalid uuid suffix")
 
     return nqn
 
