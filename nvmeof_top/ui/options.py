0ecbcee89f15472590148112e681e4a593d653f1
Paul Cuzner <pcuzner@ibm.com>
2024-03-18 11:33:08 +1300

    ui: Update Options page to include pulldown component
    Instead of a error prone text field for the nqn, the
    subsystem field is now a pulldown based on the nqn's
    discovered during the collector intialise method.
    
    In addition minor changes made to better support mypy
    
    Signed-off-by: Paul Cuzner <pcuzner@ibm.com>

--- nvmeof_top/ui/options.py
+++ nvmeof_top/ui/options.py
@@ -1,7 +1,7 @@
 import urwid  # type: ignore
 import logging
-from .common import GenericComponent, BoxButton, FixedEdit
-from nvmeof_top.utils import valid_nqn
+from .common import GenericComponent, BoxButton, FixedEdit, PullDown, PullDownOptions
+# from nvmeof_top.utils import valid_nqn
 import string
 from typing import List
 
@@ -10,21 +10,20 @@ logger = logging.getLogger(__name__)
 
 class Options(GenericComponent):
 
-    def __init__(self, parent, callback):
+    def __init__(self, parent, callback) -> None:
         self.parent = parent
         self.content = []
         self.callback = callback
 
         self.radio_button_state = self.parent.sort_key
-        self.input_subsystem = None
-        self.input_delay = None
-        self.input_sort_key = None
-        self.input_read_threshold = None
-        self.input_write_threshold = None
+        self.subsystem_pulldown: PullDown
+        self.input_delay: FixedEdit
+        self.input_read_threshold: FixedEdit
+        self.input_write_threshold: FixedEdit
         self.current_subsystem = self.parent.subsystem_nqn
         self.current_delay = str(self.parent.delay)
 
-        self.container = None  # container widget holding all the content
+        self.container: urwid.ListBox  # container widget holding all the content
 
         self.error = urwid.Text(('normal', ''))
 
@@ -32,49 +31,27 @@ class Options(GenericComponent):
 
     @property
     def page_height(self) -> int:
-        return len(self.content) + 7
+        return len(self.content) + 9
 
     def _valid_options(self) -> bool:
         """Validate the options the user has provided"""
 
-        current_subsystem = self.input_subsystem.edit_text
-        try:
-            valid_nqn(current_subsystem)
-        except ValueError as err:
-            logger.error(f"user provided nqn of '{current_subsystem}' which is an invalid")
-            self.error = urwid.Text(('error', f'Invalid subsystem name (nqn) format: {str(err)}'))
-            self.current_subsystem = current_subsystem
-            self.update()
-            self.container.set_focus(3)
-            return False
-
-        if current_subsystem not in self.parent.collector.nqn_list:
-            logger.error(f"user provided nqn of '{current_subsystem}' does not exist on this gateway")
-            self.error = urwid.Text(('error', 'Subsystem is not defined to this gateway'))
-            self.current_subsystem = current_subsystem
-            self.update()
-            self.container.set_focus(3)
-            return False
-
-        if len(self.input_delay.edit_text) == 0:
-            logger.info("user provided a null value for delay, which is invalid!")
-
-            self.error = urwid.Text(('error', 'Delay must be provided'))
-            self.current_delay = self.input_delay.edit_text
-            self.update()
-            self.container.set_focus(5)
-            return False
-
-        if int(self.input_delay.edit_text) < 1:
-            logger.info("user supplied delay of 0 makes no sense, it must be >= 1")
-
-            self.error = urwid.Text(('error'), 'Delay must be set to 1 or higher')
+        if not self.valid_delay():
+            logger.info(f"user supplied an invalid delay value: {self.input_delay.edit_text}")
+            self.error = urwid.Text(('error', "'Delay' must be set a positive number > 0"))
             self.current_delay = self.input_delay.edit_text
             self.update()
             self.container.set_focus(5)
             return False
 
         self.error = urwid.Text(('normal', ''))
+        return True
+
+    def valid_delay(self):
+        if len(self.input_delay.edit_text) == 0:
+            return False
+        if int(self.input_delay.edit_text) < 1:
+            return False
         return True
 
     def update_parent(self, component) -> None:
@@ -85,12 +62,13 @@ class Options(GenericComponent):
         logger.info("Updating options")
         logger.debug(f"radio button {self.radio_button_state}")
         data = {
-            'subsystem': self.input_subsystem.edit_text,
+            'subsystem': self.subsystem_pulldown.selected_option,
             'delay': int(self.input_delay.edit_text),
             'sort_key': self.radio_button_state,
             'read_latency_threshold': int(self.input_read_threshold.edit_text),
             'write_latency_threshold': int(self.input_write_threshold.edit_text),
         }
+        self.container.set_focus(3)
         self.callback(data)
 
     def update_sort_key(self, component, new_state, data) -> None:
@@ -106,12 +84,14 @@ class Options(GenericComponent):
 
     def _build_widget(self) -> urwid.Widget:
         self.input_delay = FixedEdit(caption="Refresh Delay: ", edit_text=self.current_delay, width=2, valid_chars=string.digits)
-        self.input_subsystem = FixedEdit(caption="Subsystem: ", edit_text=self.current_subsystem, multiline=False, align='left', allow_tab=False, width=40)
         self.input_read_threshold = FixedEdit(caption="Read Latency (ms): ", edit_text=str(self.parent.read_latency_threshold), width=2, valid_chars=string.digits)
         self.input_write_threshold = FixedEdit(caption="Write Latency (ms): ", edit_text=str(self.parent.write_latency_threshold), width=2, valid_chars=string.digits)
         sort_options: List[urwid.RadioButton] = []
         for key in self.parent.text_headers:
             urwid.RadioButton(sort_options, label=key, on_state_change=self.update_sort_key, user_data=key)
+
+        options = PullDownOptions(options_list=self.parent.collector.nqn_list)
+        self.subsystem_pulldown = PullDown(options)
 
         # reminder set_focus on self.container uses the index position of the widget in this list
         self.content = [
@@ -119,7 +99,11 @@ class Options(GenericComponent):
             urwid.Text("To change runtime options, make the required changes below and press 'Apply'. "
                        "To abort your changes simply press 'esc'."),
             urwid.Divider(),
-            self.input_subsystem,
+            urwid.Columns([
+                (12, urwid.Text('\nSubsystem: ')),
+                (options.width, urwid.LineBox(self.subsystem_pulldown)),
+                urwid.Text('')
+            ]),
             urwid.Divider(),
             self.input_delay,
             urwid.Divider(),
@@ -136,11 +120,9 @@ class Options(GenericComponent):
             urwid.GridFlow(cells=sort_options, cell_width=20, h_sep=2, v_sep=0, align='left'),
             urwid.Divider(),
             urwid.Columns([
-                urwid.Text(''),
+                self.error,
                 (9, urwid.AttrMap(BoxButton('Apply', on_press=self.update_parent), 'box-button', 'box-button-highlight'))
             ]),
-            urwid.Divider(),
-            self.error
         ]
 
         self.container = urwid.ListBox(self.content)
