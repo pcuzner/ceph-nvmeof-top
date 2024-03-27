d4830013e99bd558a185f1f09302838e7e6be49f
Paul Cuzner <pcuzner@ibm.com>
2024-03-28 09:55:08 +1300

    core: add a default minimum version
    set a default so we can do version checking
    
    Signed-off-by: Paul Cuzner <pcuzner@ibm.com>

--- nvmeof_top/defaults.py
+++ nvmeof_top/defaults.py
@@ -1,6 +1,8 @@
 import os
+from packaging import version
 
 delay = 3
 server_addr = os.environ.get('SERVER_ADDR', '')
 server_port = os.environ.get('SERVER_PORT', 5500)
 log_level = 'info'
+min_version = version.Version('1.0.0')
